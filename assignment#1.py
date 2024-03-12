80bc54bec17907e5b566362ff0a57cff18025d46
AliMurad-codes <162787735+AliMurad-codes@users.noreply.github.com>
2024-03-12 10:52:22 -0700

    Add files via upload

---
+++ assignment#1.py
@@ -0,0 +1,19 @@
+name = input("name")
+age=input('age')
+color=input("color")
+height=input('height')
+
+def eassy( name,age,color,height):
+	
+	print("my name is:",name)
+	
+	print("my age is:",age)
+	
+	print("my color is:",color)
+	
+	print("my height is:",height)
+	return name,age,color,height
+
+print (eassy( name,age,color,height))
+
+
